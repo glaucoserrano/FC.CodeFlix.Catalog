25865e9e5f40d3ca50e1a963e13db510d49ae206
glaucoserrano <glaucoserrano@gmail.com>
2022-09-09 17:36:36 -0300

    Create WebCustomFactory

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/CreateCategory/CreateCategoryApiTest.cs
@@ -0,0 +1,42 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.Commom;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using FluentAssertions;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Category.CreateCategory;
+[Collection(nameof(CreateCategoryApiTestFixture))]
+public class CreateCategoryApiTest
+{
+    private readonly CreateCategoryApiTestFixture _fixture;
+
+    public CreateCategoryApiTest(CreateCategoryApiTestFixture fixture) => _fixture = fixture;
+
+    [Fact(DisplayName = (nameof(CreateCategory)))]
+    [Trait("EndToEnd/API","Category Endpoints")]
+    public async Task CreateCategory()
+    {
+        var input = _fixture.getExampleInput();
+
+        CategoryModelOutput output = await _fixture.ApiClient
+            .Post<CategoryModelOutput>(
+                "/categories",
+                input
+             );
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Id.Should().NotBeEmpty();
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+
+        DomainEntity.Category dbCategory = await _fixture.Persistence.GetById(output.Id);
+
+        dbCategory.Should().NotBeNull();
+        dbCategory.Name.Should().Be(input.Name);
+        dbCategory.Id.Should().NotBeEmpty();
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be(input.IsActive);
+        dbCategory.CreatedAt.Should().NotBeSameDateAs(default);
+
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Base/BaseFixture.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using FC.Codeflix.Catalog.Infra.DataEF;
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Base;
+public class BaseFixture
+{
+    protected Faker Faker { get; set; }
+    public BaseFixture()
+        => Faker = new Faker("pt_BR");
+
+    public CodeflixCatalogDbContext CreateDbContext(bool preserveData = false)
+    {
+        var context = new CodeflixCatalogDbContext(
+           new DbContextOptionsBuilder<CodeflixCatalogDbContext>()
+           .UseInMemoryDatabase("end2end-test-db")
+           .Options
+           );
+        return context;
+    }
+
+}
